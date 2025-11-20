42fdbaa1d42ced91de08ff997403e6b3a1cae3fb
bureerak <pongphon_brr@hotmail.com>
2025-11-20 12:42:01 +0000

    Auto Update 2025-11-20 12:42:01

--- Tongla/Tongla.js
+++ Tongla/Tongla.js
@@ -6,20 +6,20 @@ export const systemData = {
   cpuUsage: 4.8,
   memory: {
     total: 3902,
-    used: 360
+    used: 361
   },
   storage: {
     totalGB: 18.60,
     usedGB: 6.56,
     percent: 35.29
   },
-  lastUpdated: "2025-11-20 19:41:28 ICT",
+  lastUpdated: "2025-11-20 19:42:01 ICT",
   processes: `
     PID COMMAND         %CPU %MEM
      25 kworker/1:0-eve  0.2  0.0
    1759 kworker/0:0-eve  0.2  0.0
     813 vmtoolsd         0.1  0.2
    1471 sshd             0.1  0.1
-   7875 wget             0.0  0.2
+   7482 kworker/u4:2-fl  0.0  0.0
   `
 };
