0fdb9f65cf69c875e84482e1b0446ad6dc03e334
bureerak <pongphon_brr@hotmail.com>
2025-11-20 11:35:01 +0000

    Auto Update 2025-11-20 11:35:01

--- Tongla/Tongla.js
+++ Tongla/Tongla.js
@@ -3,9 +3,9 @@ const systemStats = {
   ip: "172.16.36.128",
   os: "Ubuntu 24.04.3 LTS",
   kernel: "6.8.0-87-generic",
-  cpuUsage: 100,
-  memoryUsage: 8,
+  cpuUsage: 4.8,
+  memoryUsage: 9,
   diskUsage: "38",
-  lastUpdated: "2025-11-20 18:34:01 ICT",
-  processes: [{"pid":25, "name":"kworker/1:0-eve", "cpu":0.2, "mem":0.0},{"pid":1759, "name":"kworker/0:0-eve", "cpu":0.2, "mem":0.0},{"pid":1471, "name":"sshd", "cpu":0.2, "mem":0.1},{"pid":813, "name":"vmtoolsd", "cpu":0.1, "mem":0.2},{"pid":3893, "name":"kworker/u4:3-fl", "cpu":0.0, "mem":0.0}]
+  lastUpdated: "2025-11-20 18:35:01 ICT",
+  processes: [{"pid":4572, "name":"python3", "cpu":0.5, "mem":0.4},{"pid":25, "name":"kworker/1:0-eve", "cpu":0.2, "mem":0.0},{"pid":1759, "name":"kworker/0:0-eve", "cpu":0.2, "mem":0.0},{"pid":1471, "name":"sshd", "cpu":0.2, "mem":0.1},{"pid":813, "name":"vmtoolsd", "cpu":0.1, "mem":0.2}]
 };
